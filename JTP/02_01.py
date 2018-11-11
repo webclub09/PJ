6ac86a07b5a4a2818bccd19385766b10cbcce6c6
webclub09 <webclub09@gmail.com>
2018-11-11 19:14:02 +0900

    01

---
+++ JTP/02_01.py
@@ -0,0 +1,24 @@
+# 파이참 단축키    맥       ,     windows
+# 주석달기 command + /   cnt + /
+# console에서 바로 시작하기 alt+shift+E
+
+
+
+# 정수형
+a = 123
+a = -178
+a = 0
+
+# 실수형
+a = 1.2
+a = -3.45
+
+
+
+head = "Python"
+tall = " is fun"
+
+head + tall
+
+
+
---
+++ JTP/02_02.py
@@ -0,0 +1,26 @@
+# 숫자 바로 대입
+"I eat %d apples." %3
+
+# 문자 바로 대입
+"I eat %s apples." %"five"
+
+# 숫자 ㄱ밧을 나타내는 변수로 대입
+number = 3
+"I eat %d apples." %number
+
+#2개 이상의 값 넣기
+number = 3
+day = "three"
+"I eat %d apples. %s days " %(number, day)
+
+
+#### 문자열 포멧코드
+# %s  문자열(string)
+# %c  문자 1개(charactor)
+# %d  정수
+# %f  부동 소수
+# %o  8진수
+# %x  16진수
+# %%  Literal % (문자 '%' 자체)
+
+
